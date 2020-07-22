a4762f4711bdcc5ffc9d859e091688e6b8267df4
shadyzoz <shadyzoz@163.com>
2020-07-22 11:07:28 +0800

    adjust typings

--- typings/index.d.ts
+++ typings/index.d.ts
@@ -1,17 +1,18 @@
-interface FCRequest {
+declare module "@shadyzoz/fc-helper" {
+  interface FCRequest {
     path: string;
     method: string;
     headers: any;
     query: any;
-}
+  }
 
-interface FCResponse {
+  interface FCResponse {
     statusCode: number;
     headers: any;
     body: any;
-}
+  }
 
-interface Context {
+  interface Context {
     req: FCRequest;
     res: FCResponse;
     path: string;
@@ -20,47 +21,47 @@ interface Context {
     status: number;
     body?: any;
     set(key: string, value: string);
-}
+  }
 
-interface FCMiddleWare {
+  interface FCMiddleWare {
     (ctx: Context): Promise<any>;
-}
+  }
 
-interface FCHTTPEvent {
+  interface FCHTTPEvent {
 
-}
+  }
 
-interface Credentials {
+  interface Credentials {
     accessKeyId?: string;
     accessKeySecret?: string;
     securityToken?: string;
-}
+  }
 
-interface Functions {
+  interface Functions {
     name?: string;
     handler?: string;
     memory?: number;   // Integer, in MB.
     timeout?: number;      // Integer, in seconds.
-}
+  }
 
-interface Service {
+  interface Service {
     name: string;
     logProject: string;
     logStore: string;
     qualifier: string;
     versionId: string;
-}
+  }
 
-interface FCContext {
+  interface FCContext {
     requestId?: string;
     credentials?: Credentials;
     "function"?: Functions;
     service?: Service;
     region: string;
     accountId: string;
-}
+  }
 
-interface FCRAWRequestEvent {
+  interface FCRAWRequestEvent {
     path: String;
     httpMethod: String;
     headers?: any;
@@ -68,27 +69,26 @@ interface FCRAWRequestEvent {
     pathParameters?: any
     body?: String;
     isBase64Encoded?: boolean
-}
+  }
 
-interface FCRAWResponse {
+  interface FCRAWResponse {
     isBase64Encoded?: boolean;
     statusCode?: number;
     headers?: any;
     body?: string
-}
+  }
 
-interface Tester {
+  interface Tester {
     mockFromConfig(serviceName: string, functionName: string, templatePath?: string): Tester
     run(event: FCRAWRequestEvent, ctx?: FCContext): Promise<FCRAWResponse>
     run(event: Buffer, ctx?: FCContext): Promise<any>
-}
+  }
 
-type AsyncRequestFunc = (event: FCRAWRequestEvent, ctx: FCContext) => Promise<FCRAWResponse>;
-type AsyncEventFunc = (event: Buffer, ctx: FCContext) => Promise<any>;
+  type AsyncRequestFunc = (event: FCRAWRequestEvent, ctx: FCContext) => Promise<FCRAWResponse>;
+  type AsyncEventFunc = (event: Buffer, ctx: FCContext) => Promise<any>;
 
-declare module "@shadyzoz/fc-helper" {
-    function hook(middleWare: FCMiddleWare)
-    function test(middleWare: FCMiddleWare): Tester
-    function asyncWrap(asyncRequestFunc: AsyncRequestFunc): any
-    function asyncWrap(asyncEventFunc: AsyncEventFunc): any
+  function hook(middleWare: FCMiddleWare)
+  function test(middleWare: FCMiddleWare): Tester
+  function asyncWrap(asyncRequestFunc: AsyncRequestFunc): any
+  function asyncWrap(asyncEventFunc: AsyncEventFunc): any
 }
